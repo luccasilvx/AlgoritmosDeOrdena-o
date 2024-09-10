f7af3c8af6c95c6f2b552ca5ae35ba87f82e37f4
luccasilvx <lucafreitasilva@gmail.com>
2024-09-10 16:00:46 -0300

    subindo BubbleSort

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] vetor = new int[5];
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Digite o "+(i+1)+"° numero: ");
+            vetor[i] = scanner.nextInt();
+        }
+        bubbleSortDescrescente(vetor);
+        System.out.println("Vetor ordenado em ordem crescente:");
+        for (int i = 0; i < 5;i++){
+            System.out.println((i+1) + "° numero: "+vetor[i]);
+        }
+
+    }
+    public static void bubbleSortDescrescente(int[] vetor){
+        int numero = vetor.length;
+        boolean troca;
+
+        do{
+            troca = false;
+            for (int i= 0; i < numero - 1;i++){
+                if (vetor[i] <vetor[i+1]){
+                    int auxilia = vetor[i];
+                    vetor[i] = vetor[i+1];
+                    vetor[i+1] = auxilia;
+                    troca = true;
+                }
+            }
+            numero--;
+        }while(troca);
+    }
 }
+
